d86411f6237a9f4829acab9e05dd18c5ae13ae14
Oleksii <oleksii.lihachov@skynix.co>
2020-02-28 20:44:28 +0200

    Adjustments on Accordion Block

--- wp-content/plugins/rri-custom-blocks/src/block/hero-slider/edit.js
+++ wp-content/plugins/rri-custom-blocks/src/block/hero-slider/edit.js
@@ -70,21 +70,22 @@ addFilter('stackable.hero-slider.edit.inspector.style.before', 'stackable/hero-s
 						if (slides_data.length < value) {
 							slider_data_clone.push(
 								{
-									title: __('Lorem et dolor ipsum', i18n),
-									copy: __('Lorem ipsum dolor sit amet, consectetur adipiscing elit aenean vehicula lacus sit amet fringilla.', i18n),
+									title: '',
+									copy: '',
 									image: {
 										url: 'https://local.test.com/wp-content/uploads/2020/02/Image.png',
 										id: ''
 									},
-									button: {
-										url: 'https://google.com',
-										newTab: true,
-										noFollow: false,
-										text: 'Buy Now'
-									},
 									params: {
-										buttonSize: 'small',
 										align: 'left'
+									},
+									button: {
+										url: '',
+										newTab: false,
+										noFollow: false,
+										text: '',
+										design: 'primary',
+										size: 'small'
 									}
 								}
 							);
@@ -159,25 +160,11 @@ addFilter('stackable.hero-slider.edit.inspector.style.before', 'stackable/hero-s
 			</PanelBody>
 
 			{slides_data.map((item, index) => {
-				const {buttonSize, align} = item.params;
+				const {button} = item;
+				const {align} = item.params;
 
 				return (
 					<PanelBody title={__(`Slide ${index + 1}`, i18n)} initialOpen={false}>
-						<SelectControl label={__('Button Size', i18n)}
-									   options={[
-										   {value: 'small', label: __('Small', i18n)},
-										   {value: 'medium', label: __('Medium', i18n)},
-										   {value: 'large', label: __('Large', i18n)}
-									   ]}
-									   value={buttonSize}
-									   onChange={(value) => {
-										   const slides_data_clone = cloneDeep(slides_data);
-										   slides_data_clone[index].params.buttonSize = value;
-										   setAttributes({
-											   slides_data: slides_data_clone
-										   });
-									   }}
-						/>
 						<SelectControl label={__('Align Text', i18n)}
 									   options={[
 										   {value: 'left', label: __('Left', i18n)},
@@ -188,6 +175,35 @@ addFilter('stackable.hero-slider.edit.inspector.style.before', 'stackable/hero-s
 									   onChange={(value) => {
 										   const slides_data_clone = cloneDeep(slides_data);
 										   slides_data_clone[index].params.align = value;
+										   setAttributes({
+											   slides_data: slides_data_clone
+										   });
+									   }}
+						/>
+						<SelectControl label={__('Design', i18n)}
+									   options={[
+										   {value: 'primary', label: __('Primary', i18n)},
+										   {value: 'secondary', label: __('Secondary', i18n)}
+									   ]}
+									   value={button.design}
+									   onChange={(value) => {
+										   const slides_data_clone = cloneDeep(slides_data);
+										   slides_data_clone[index].button.design = value;
+										   setAttributes({
+											   slides_data: slides_data_clone
+										   });
+									   }}
+						/>
+						<SelectControl label={__('Size', i18n)}
+									   options={[
+										   {value: 'small', label: __('Small', i18n)},
+										   {value: 'medium', label: __('Medium', i18n)},
+										   {value: 'large', label: __('Large', i18n)}
+									   ]}
+									   value={button.size}
+									   onChange={(value) => {
+										   const slides_data_clone = cloneDeep(slides_data);
+										   slides_data_clone[index].button.size = value;
 										   setAttributes({
 											   slides_data: slides_data_clone
 										   });
@@ -210,35 +226,13 @@ class Edit extends Component {
 			openUrlPopover: false
 		};
 		this.sliderRef = createRef();
-		this.onChangeUrl = this.onChangeUrl.bind(this);
-		this.onChangeNewTab = this.onChangeNewTab.bind(this);
-		this.onChangeNoFollow = this.onChangeNoFollow.bind(this);
+		this.decreasesSlides = false;
+		this.handleFocusOutside = this.handleFocusOutside.bind(this)
 	}
 
-	onChangeUrl(value, index) {
-		const {setAttributes, attributes} = this.props;
-		const slides_data_clone = cloneDeep(attributes.slides_data);
-		slides_data_clone[index].button.url = value;
-		setAttributes({
-			slides_data: slides_data_clone
-		});
-	}
-
-	onChangeNewTab(value, index) {
-		const {setAttributes, attributes} = this.props;
-		const slides_data_clone = cloneDeep(attributes.slides_data);
-		slides_data_clone[index].button.newTab = value;
-		setAttributes({
-			slides_data: slides_data_clone
-		});
-	}
-
-	onChangeNoFollow(value, index) {
-		const {setAttributes, attributes} = this.props;
-		const slides_data_clone = cloneDeep(attributes.slides_data);
-		slides_data_clone[index].button.noFollow = value;
-		setAttributes({
-			slides_data: slides_data_clone
+	handleFocusOutside() {
+		this.setState({
+			openUrlPopover: null,
 		});
 	}
 
@@ -290,70 +284,69 @@ class Edit extends Component {
 				blockProps={this.props}
 				render={() => (
 					<div className="rri-hero-slider__wrapper" ref={this.sliderRef}>
-						<div className="rri-hero-slider__inner"
-							 onMouseDown={() => {
-								 this.setState({openUrlPopover: false})
-							 }}>
+						<div className="rri-hero-slider__inner">
 							{slides_data.map((item, index) => {
 								const {title, copy, button} = item;
-								const {buttonSize, align} = item.params;
-								const ctaClasses = classnames(['rri-hero-slide__cta', `rri-hero-slide__cta--${buttonSize}`]);
+								const {align} = item.params;
 								const slideClasses = classnames(['rri-hero-slide', `rri-hero-slide--${align}`]);
+								const ctaClasses = classnames([
+									'rri-hero-slide__cta',
+									`rri-hero-slide__cta--${button.size}`,
+									`rri-hero-slide__cta--${button.design}`
+								]);
 
 								return (
-									<div className={slideClasses} style={{
-										backgroundImage: `url(${item.image.url})`
-									}}>
-										<div className="rri-hero-slide__content">
-											<RichText
-												tagName="h2"
-												className="rri-hero-slide__title"
-												value={title}
-												onChange={(value) => {
-													const slider_data_clone = cloneDeep(slides_data);
-													slider_data_clone[index].title = value;
-													setAttributes({
-														slides_data: slider_data_clone
-													});
-												}}
-											/>
-											<RichText
-												tagName='p'
-												className="rri-hero-slide__copy"
-												value={copy}
-												onChange={(value) => {
-													const slider_data_clone = cloneDeep(slides_data);
-													slider_data_clone[index].copy = value;
-													setAttributes({
-														slides_data: slider_data_clone
-													});
-												}}
-											/>
-											<div className={ctaClasses} onClick={() => {
-												this.setState({
-													openUrlPopover: index
-												});
-											}}>
+									<div className={slideClasses}
+										 style={{
+											 backgroundImage: `url(${item.image.url})`
+										 }}>
+										<div className="rri-hero-slide__wrapper">
+											<div className="rri-hero-slide__content">
 												<RichText
-													tagName="span"
-													className="rri-hero-slide__cta-text"
-													value={button.text}
+													tagName="h2"
+													placeholder={__('Title', i18n)}
+													className="rri-hero-slide__title"
+													value={title}
 													onChange={(value) => {
 														const slider_data_clone = cloneDeep(slides_data);
-														slider_data_clone[index].button.text = value;
+														slider_data_clone[index].title = value;
 														setAttributes({
 															slides_data: slider_data_clone
 														});
 													}}
+													keepPlaceholderOnFocus
 												/>
-												{this.state.openUrlPopover === index && <UrlInputPopover
-													value={button.url}
-													newTab={button.newTab}
-													noFollow={button.noFollow}
-													onChange={value => this.onChangeUrl(value, index)}
-													onChangeNewTab={value => this.onChangeNewTab(value, index)}
-													onChangeNoFollow={value => this.onChangeNoFollow(value, index)}
-												/>}
+												<RichText
+													tagName='p'
+													placeholder={__('Copy', i18n)}
+													className="rri-hero-slide__copy"
+													value={copy}
+													onChange={(value) => {
+														const slider_data_clone = cloneDeep(slides_data);
+														slider_data_clone[index].copy = value;
+														setAttributes({
+															slides_data: slider_data_clone
+														});
+													}}
+													keepPlaceholderOnFocus
+												/>
+												<div className={ctaClasses}
+													 onClick={() => this.setState({openUrlPopover: index})}>
+													<RichText
+														tagName="span"
+														placeholder={__('Link Text', i18n)}
+														className="rri-hero-slide__cta-text"
+														value={button.text}
+														onChange={(value) => {
+															const slider_data_clone = cloneDeep(slides_data);
+															slider_data_clone[index].button.text = value;
+															setAttributes({
+																slides_data: slider_data_clone
+															});
+														}}
+														keepPlaceholderOnFocus
+													/>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -376,12 +369,12 @@ export default compose(
 	withSetAttributeHook,
 	withTabbedInspector(),
 	withBlockStyles(createStyles, {editorMode: true}),
-	withSelect((select, {clientId}) => {
-		const {getBlock} = select('core/block-editor');
-		const block = getBlock(clientId);
-		return {
-			hasInnerBlocks: !!(block && block.innerBlocks.length),
-		}
-	}),
+	// withSelect((select, {clientId}) => {
+	// 	const {getBlock} = select('core/block-editor');
+	// 	const block = getBlock(clientId);
+	// 	return {
+	// 		hasInnerBlocks: !!(block && block.innerBlocks.length),
+	// 	}
+	// }),
 	withFocusOutside,
 )(Edit);
